d098857d8cc2804fcf051c6286ea53c13c3bb304
Hugo van de Vliert <hugo.vandevliert@floryn.com>
2025-07-16 13:58:49 +0200

    Use nerd font variant of JetBrains Mono

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -120,14 +120,15 @@ return {
         severity_sort = true,
         float = { border = 'rounded', source = 'if_many' },
         underline = { severity = vim.diagnostic.severity.ERROR },
-        signs = vim.g.have_nerd_font and {
+        virtual_text = { current_line = true },
+        signs = {
           text = {
             [vim.diagnostic.severity.ERROR] = '󰅚 ',
             [vim.diagnostic.severity.WARN] = '󰀪 ',
             [vim.diagnostic.severity.INFO] = '󰋽 ',
             [vim.diagnostic.severity.HINT] = '󰌶 ',
           },
-        } or {},
+        },
       }
 
       -- Enable the following language servers
--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -27,8 +27,7 @@ return  {
     --  You could remove this setup call if you don't like it,
     --  and try some other statusline plugin
     local statusline = require 'mini.statusline'
-    -- set use_icons to true if you have a Nerd Font
-    statusline.setup { use_icons = vim.g.have_nerd_font }
+    statusline.setup { use_icons = true }
 
     -- You can configure sections in the statusline by overriding their
     -- default behavior. For example, here we set the section for

e9c2f15a3c5d6867664ea077b35a2aa09600c44b
Hugo van de Vliert <hugo.vandevliert@floryn.com>
2025-07-21 22:51:49 +0200

    Fix highlighting in ruby yaml files

--- nvim/lua/plugins/rails.lua
+++ nvim/lua/plugins/rails.lua
@@ -1,7 +1,15 @@
 -- https://github.com/tpope/vim-rails
 
-vim.g.rails_path_additions = { 'app/*/*' }
-
 return {
   'tpope/vim-rails',
+  config = function()
+    -- Add custom path additions for Rails
+    vim.g.rails_path_additions = { 'app/*/*' }
+
+    -- eruby.yml files are not supported by treesitter
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'eruby.yaml',
+      command = 'set filetype=yaml',
+    })
+  end,
 }

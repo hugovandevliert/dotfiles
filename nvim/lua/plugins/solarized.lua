c20d13567eea8b609a8b1c4616d03e279b0bd23d
Hugo van de Vliert <hvandevliert@icloud.com>
2025-07-23 20:49:13 +0200

    Add note about background color bug

--- nvim/lua/plugins/solarized.lua
+++ nvim/lua/plugins/solarized.lua
@@ -1,8 +1,12 @@
 -- https://github.com/lifepillar/vim-solarized8
+--
+-- Note: there's currently a bug in neovim that causes the colorscheme to load
+-- twice on launch, which can cause flickering since it defaults to a dark theme.
+-- See: https://github.com/neovim/neovim/issues/32109
 
 return {
   'lifepillar/vim-solarized8',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
+  priority = 1000,
   config = function()
     vim.cmd.colorscheme 'solarized8_flat'
   end,

c934ecdcea049c735f28d409c64c88f095f9ed2b
Hugo van de Vliert <hvandevliert@icloud.com>
2025-08-03 22:04:53 +0200

    Fix codecompanion setup

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -8,6 +8,8 @@ return {
     'nvim-treesitter/nvim-treesitter',
   },
   config = function()
+    require('codecompanion').setup()
+
     vim.keymap.set('n', '<Leader>a', '<cmd>CodeCompanionChat Toggle<cr>', { silent = true, desc = 'Toggle [A]I chat' })
     vim.keymap.set('v', 'ga', '<cmd>CodeCompanionChat Add<cr>', { silent = true, desc = 'Add selection to [A]I chat' })
 

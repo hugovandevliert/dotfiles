57bb469963966c8dc6db49b22125f2207a54d3ad
Hugo van de Vliert <hugo.vandevliert@floryn.com>
2025-08-04 22:40:10 +0200

    Disable spell checking in man pages

--- nvim/init.lua
+++ nvim/init.lua
@@ -187,6 +187,14 @@ vim.api.nvim_create_autocmd('TermOpen', {
   end,
 })
 
+-- Disable spell checking in man page buffers
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'man',
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'

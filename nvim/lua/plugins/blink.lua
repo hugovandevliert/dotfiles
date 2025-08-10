ebbeac4526bc498efb47ef8799f091ed16fe7e93
Hugo van de Vliert <hvandevliert@icloud.com>
2025-08-10 22:19:23 +0200

    Configure snippets for rails

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,4 +1,3 @@
- -- Autocompletion
  -- https://github.com/Saghen/blink.cmp
 
 return {
@@ -17,12 +16,13 @@ return {
         -- `friendly-snippets` contains a variety of premade snippets.
         --    See the README about individual language/framework/plugin snippets:
         --    https://github.com/rafamadriz/friendly-snippets
-        -- {
-        --   'rafamadriz/friendly-snippets',
-        --   config = function()
-        --     require('luasnip.loaders.from_vscode').lazy_load()
-        --   end,
-        -- },
+        {
+          'rafamadriz/friendly-snippets',
+          config = function()
+            require('luasnip.loaders.from_vscode').lazy_load()
+            require('luasnip').filetype_extend('ruby', {'rails'})
+          end,
+        },
       },
       opts = {},
     },
@@ -32,12 +32,6 @@ return {
   --- @type blink.cmp.Config
   opts = {
     keymap = { preset = 'default' },
-
-    appearance = {
-      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'mono',
-    },
 
     completion = {
       -- By default, you may press `<c-space>` to show the documentation.
@@ -56,15 +50,6 @@ return {
     },
 
     snippets = { preset = 'luasnip' },
-
-    -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
-    -- which automatically downloads a prebuilt binary when enabled.
-    --
-    -- By default, we use the Lua implementation instead, but you may enable
-    -- the rust implementation via `'prefer_rust_with_warning'`
-    --
-    -- See :h blink-cmp-config-fuzzy for more information
-    fuzzy = { implementation = 'lua' },
 
     -- Shows a signature help window while you type arguments for a function
     -- NOTE: Disabled for now since it conflicts with copilot's <C-k> mapping

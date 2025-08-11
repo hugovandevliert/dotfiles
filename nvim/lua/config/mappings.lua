4e2fbaabecbe320a863991e544edb9aa8198ac2d
Hugo van de Vliert <hugo.vandevliert@floryn.com>
2025-08-11 23:18:50 +0200

    Split up init.lua file into separate config files

---
+++ nvim/lua/config/mappings.lua
@@ -0,0 +1,55 @@
+-- Quick saving and quitting
+vim.keymap.set('n', '<leader>w', '<cmd>w<cr>', { desc = '[W]rite'})
+vim.keymap.set('n', '<leader>q', '<cmd>confirm q<cr>', { desc = '[Q]uit'})
+vim.keymap.set('n', '<leader>x', '<cmd>x<cr>', { desc = '[X] Write and quit'})
+
+-- Sane movement when wrap is enabled
+vim.keymap.set('n', 'j', 'gj')
+vim.keymap.set('n', 'k', 'gk')
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Indent without losing selection
+vim.keymap.set('x', '<', '<gv')
+vim.keymap.set('x', '>', '>gv')
+
+-- Paste without adding to the register in visual mode
+vim.keymap.set('x', 'p', function()
+  return 'pgv"' .. vim.v.register .. 'y`>'
+end, { expr = true })
+vim.keymap.set('x', 'P', function()
+  return 'Pgv"' .. vim.v.register .. 'y`>'
+end, { expr = true })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Copy relative path to system clipboard
+vim.keymap.set('n', '<leader>p', function()
+  vim.fn.setreg('+', vim.fn.expand('%:.'))
+  vim.cmd('echo "Copied file path to clipboard."')
+end, { desc = 'Copy relative file [p]ath to clipboard', silent = true })
+
+-- Toggle spell check
+vim.keymap.set('n', '<leader>cs', '<cmd>set spell!<CR>', { desc = '[T]oggle [s]pell check', silent = true })
+
+-- Toggle colorcolumn
+vim.keymap.set('n', '<leader>cc', function()
+  vim.opt.colorcolumn = vim.opt.colorcolumn:get()[1] == '80' and '' or '80'
+end, { desc = '[C]hange [c]olorcolumn', silent = true })
+
+-- Toggle line wrap
+vim.keymap.set('n', '<leader>cw', '<cmd>set wrap!<CR>', { desc = '[C]hange line [w]rap', silent = true })
+
+-- Toggle listchars
+vim.keymap.set('n', '<leader>cl', ':set list!<CR>', { desc = '[C]hange [l]ist chars', silent = true })
---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,92 @@
+-- Make line numbers default
+vim.o.number = true
+
+-- Minimal number of screen lines to keep above and below the cursor
+vim.o.scrolloff = 4
+
+-- Keep a few characters visible to the left and right of the cursor
+vim.o.sidescrolloff = 4
+
+-- Scroll left/right one character at a time
+vim.o.sidescroll = 1
+
+-- Don't show the mode, since it's already in the status line
+vim.o.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Assume the /g flag on :s substitutions
+vim.o.gdefault = true
+
+-- Disable swap files
+vim.o.swapfile = false
+
+-- Save undo history
+vim.o.undofile = true
+vim.o.undodir = vim.fs.normalize('~/.config/nvim/undo')
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.o.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 200
+vim.o.timeoutlen = 200
+
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+-- Display certain whitespace characters
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Use two spaces for a tab
+vim.o.smarttab = true
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+
+-- Preview substitutions live, as you type!
+vim.o.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.o.cursorline = true
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.o.confirm = true
+
+-- Enable spell checking for English and Dutch
+vim.o.spell = true
+vim.o.spelllang = 'en,nl'
+
+-- Disable wrap by default
+vim.o.wrap = false
+
+-- Delete comment character when joining commented lines
+vim.opt.formatoptions:append('j')
+
+-- Don't include newline characters in visual mode
+vim.o.selection = 'old'
+
+-- Allow virtual editing in block mode
+vim.o.virtualedit = 'block'
+
+-- Configure folding with Treesitter
+vim.o.foldmethod = 'expr'
+vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.o.foldtext = ''
+vim.o.foldlevel = 99

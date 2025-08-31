b75c32d8499cfa0ef9f90c624e26dec6a4a48ab0
Hugo van de Vliert <hugo.vandevliert@floryn.com>
2025-08-31 16:42:19 +0200

    [editor] Disable snippets completion

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -5,27 +5,6 @@ return {
   event = 'VimEnter',
   version = '1.*',
   dependencies = {
-    -- Snippet Engine
-    {
-      'L3MON4D3/LuaSnip',
-      version = '2.*',
-      build = (function()
-        return 'make install_jsregexp'
-      end)(),
-      dependencies = {
-        -- `friendly-snippets` contains a variety of premade snippets.
-        --    See the README about individual language/framework/plugin snippets:
-        --    https://github.com/rafamadriz/friendly-snippets
-        {
-          'rafamadriz/friendly-snippets',
-          config = function()
-            require('luasnip.loaders.from_vscode').lazy_load()
-            require('luasnip').filetype_extend('ruby', { 'rails' })
-          end,
-        },
-      },
-      opts = {},
-    },
     'folke/lazydev.nvim',
   },
   --- @module 'blink.cmp'
@@ -50,8 +29,6 @@ return {
         lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
       },
     },
-
-    snippets = { preset = 'luasnip' },
 
     -- Shows a signature help window while you type arguments for a function
     -- NOTE: Disabled for now since it conflicts with copilot's <C-k> mapping

e04773c5bf6d4fdf2dafdc289c2b39d670c21658
Hugo van de Vliert <hugo.vandevliert@floryn.com>
2025-10-14 14:55:21 +0200

    [editor] Configure CodeCompanion to look for instructions

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -8,7 +8,30 @@ return {
     'nvim-treesitter/nvim-treesitter',
   },
   config = function()
-    require('codecompanion').setup()
+    require('codecompanion').setup({
+      memory = {
+        default = {
+          description = 'Collection of common files for all projects',
+          files = {
+            '.github/copilot-instructions.md',
+            'AGENT.md',
+            'AGENTS.md',
+          },
+        },
+        opts = {
+          chat = { enabled = true },
+        },
+      },
+      strategies = {
+        chat = {
+          keymaps = {
+            clear = {
+              modes = { n = 'gq' },
+            },
+          },
+        },
+      },
+    })
 
     vim.keymap.set('n', '<Leader>a', '<cmd>CodeCompanionChat Toggle<cr>', { silent = true, desc = 'Toggle [A]I chat' })
     vim.keymap.set('v', 'ga', '<cmd>CodeCompanionChat Add<cr>', { silent = true, desc = 'Add selection to [A]I chat' })

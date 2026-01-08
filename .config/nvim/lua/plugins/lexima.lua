a392826493a06484341664269f651065255c8033
Hugo van de Vliert <hugo.vandevliert@floryn.com>
2026-01-08 22:53:21 +0100

    [editor] Autoclose ERB tags

--- .config/nvim/lua/plugins/lexima.lua
+++ .config/nvim/lua/plugins/lexima.lua
@@ -2,4 +2,13 @@
 
 return {
   'cohama/lexima.vim',
+  config = function()
+    local erb = { 'eruby', 'erb', 'ruby' }
+
+    -- ERB tag autocompletion: <% %>, <%= %>, <%# %>
+    vim.fn['lexima#add_rule']({ char = '%', at = '<\\%#', input_after = '%>', filetype = erb })
+
+    -- Space adds padding on both sides: <% | %>, <%= | %>
+    vim.fn['lexima#add_rule']({ char = '<Space>', at = '<%.*\\%#%>', input_after = ' ', filetype = erb })
+  end,
 }

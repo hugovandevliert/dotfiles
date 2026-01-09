ad94387f7ed96e7bec93208e96125aa6af25cfa7
Hugo van de Vliert <hugo.vandevliert@floryn.com>
2026-01-09 21:29:03 +0100

    [editor] Disable cursor in terminal mode

--- .config/nvim/lua/plugins/solarized.lua
+++ .config/nvim/lua/plugins/solarized.lua
@@ -10,5 +10,13 @@ return {
   branch = 'neovim',
   config = function()
     vim.cmd.colorscheme('solarized8_flat')
+
+    -- Hide cursor in terminal mode so it matches the regular cursor. This needs
+    -- to be done in an autocmd because of the reloading bug mentioned above.
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      callback = function()
+        vim.api.nvim_set_hl(0, 'TermCursor', { blend = 100 })
+      end,
+    })
   end,
 }

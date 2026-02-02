1159baca0bc6d392a9d64e95a3153e7a665fbbc1
Hugo van de Vliert <hugo.vandevliert@floryn.com>
2026-02-02 14:31:52 +0100

    [editor] Add eslint LSP

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -95,6 +95,7 @@ return {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- See `:help lspconfig-all` for a list of all the pre-configured LSPs
+        eslint = {},
         lua_ls = { settings = { Lua = { diagnostics = { globals = { 'vim' } } } } },
         rubocop = {},
         ruby_lsp = {},

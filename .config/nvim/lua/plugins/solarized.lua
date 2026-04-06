747e239b6ea3fa12988134c26233b6682df92cd8
Hugo van de Vliert <hugo.vandevliert@floryn.com>
2026-04-06 13:52:56 +0200

    [editor] Update plugins

--- .config/nvim/lua/plugins/solarized.lua
+++ .config/nvim/lua/plugins/solarized.lua
@@ -1,17 +1,18 @@
--- https://github.com/lifepillar/vim-solarized8
+-- https://codeberg.org/lifepillar/vim-solarized8
 --
 -- Note: there's currently a bug in neovim that causes the colorscheme to load
 -- twice on launch, which can cause flickering since it defaults to a dark theme.
 -- See: https://github.com/neovim/neovim/issues/32109
 
 return {
-  'lifepillar/vim-solarized8',
+  'https://codeberg.org/lifepillar/vim-solarized8',
   priority = 1000,
   branch = 'neovim',
   config = function()
     vim.cmd.colorscheme('solarized8_flat')
 
     -- These need to be in an autocmd because of the reloading bug linked above.
+    -- Probably no longer necessary once https://github.com/neovim/neovim/pull/37075 is merged
     vim.api.nvim_create_autocmd('ColorScheme', {
       callback = function()
         -- Hide cursor in terminal mode so it matches the regular cursor.

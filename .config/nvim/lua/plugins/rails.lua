8bf93dbf21eb66bbb00c6e7f2941f830b0828dbf
Hugo van de Vliert <hugo.vandevliert@floryn.com>
2026-05-29 13:15:46 +0200

    [editor] Jump between controllers and request specs

--- .config/nvim/lua/plugins/rails.lua
+++ .config/nvim/lua/plugins/rails.lua
@@ -6,6 +6,22 @@ return {
     -- Add custom path additions for Rails
     vim.g.rails_path_additions = { 'app/*/*' }
 
+    -- Jump between controllers and their request/controller specs
+    vim.g.rails_projections = {
+      ['app/controllers/*_controller.rb'] = {
+        alternate = {
+          'spec/requests/{}_controller_spec.rb',
+          'spec/controllers/{}_controller_spec.rb',
+        },
+      },
+      ['spec/controllers/*_controller_spec.rb'] = {
+        alternate = { 'app/controllers/{}_controller.rb' },
+      },
+      ['spec/requests/*_controller_spec.rb'] = {
+        alternate = { 'app/controllers/{}_controller.rb' },
+      },
+    }
+
     -- eruby.yml files are not supported by treesitter
     vim.api.nvim_create_autocmd('FileType', {
       pattern = 'eruby.yaml',
